d51296762ad590b91dea8d8b57ef3e279690ce3d
muhammadfaatihaflah-del <muhammadfaatihaflah@gmail.com>
2026-04-06 10:36:11 +0700

    mengisi data dengan perintah satu per satu

--- struct.cpp
+++ struct.cpp
@@ -10,6 +10,21 @@ int main()
 
     //cara akses (membaca atau mengambil isi array)
     cout << "isi data kedua : "  <<dataku [1]  << endl;
-    
+    //mengisi array
+    dataku[1] = 200;
+    cout << "isi data kedua setelah dirubah : " <<dataku[1]  << endl;
 
+    //mengisi data dengan perintah satu per satu
+     cout << "Isikan data pertama : ";
+    cin >> datamu[0];
+    cout << "Isikan data kedua : ";
+    cin >> datamu[1];
+    cout << "Isikan data ketiga : ";
+    cin >> datamu[2];
+    cout << "Isikan data keempat : ";
+    cin >> datamu[3];
+    cout << "Isikan data kelima : ";
+    cin >> datamu[5];
+
+    
 }
